b6d62054d5d0b97e4870849e937ad2e6723db06d
Egor Heybye <eshestakov@alfabank.ru>
2024-06-19 04:45:07 +0700

    upd

--- src/test/java/ru/inno/certification3/Pages/MainPage.java
+++ src/test/java/ru/inno/certification3/Pages/MainPage.java
@@ -1,4 +1,52 @@
 package ru.inno.certification3.Pages;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byValue;
+import static com.codeborne.selenide.Selenide.*;
+
 public class MainPage {
+
+
+    public void checkEmptyLines() {
+        $(".rt-noData").shouldHave(text("No rows found"));
+    }
+
+    public void selectTenRows() {
+        $(byAttribute("aria-label", "rows per page")).$(byValue("20")).click();
+    }
+
+
+    public void checkRow(){
+        $$("div.rt-td-group");
+    }
+
+    public int checkBooksInEveryRow(){
+        ElementsCollection rows = $$("div.rt-tr-group");
+
+        int booksCount = 0;
+
+        for (SelenideElement row : rows) {
+
+            if (!row.$$("div.rt-td img").isEmpty()) {
+                booksCount++;
+            }
+        }
+        return booksCount;
+    }
+
+    public void deleteAllBooks(){
+        $("#submit").click();
+    }
+
+    public void acceptDeleteAllBooks(){
+        $("#closeSmallModal-ok").click();
+    }
+
+    public void acceptAlert(){
+        confirm();
+    }
 }
--- src/test/java/ru/inno/certification3/TestDemoQa.java
+++ src/test/java/ru/inno/certification3/TestDemoQa.java
@@ -1,17 +1,21 @@
 package ru.inno.certification3;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import demoQA.helper.configHelper;
 import ru.inno.certification3.Pages.LoginPage;
+import ru.inno.certification3.Pages.MainPage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDemoQa {
 
     LoginPage loginPage = new LoginPage();
+    MainPage mainPage = new MainPage();
 
     @BeforeAll
     public static void setUp(){
@@ -19,14 +23,41 @@ public class TestDemoQa {
     }
 
 
-    @Test
-    public void test() throws InterruptedException {
+    @BeforeEach
+    public void auth(){
         String url = configHelper.getUrl();
         open(url);
         loginPage.enterUsername();
         loginPage.enterPassword();
         loginPage.enterLoginButton();
-        Thread.sleep(5000);
+    }
+
+
+    @Test
+    public void checkEmptyTable() throws InterruptedException {
+        loginPage.enterLoginButton();
+        mainPage.checkEmptyLines();
+    }
+
+    @Test
+    public void checkBooksInTable() {
+        //добавить 6 книг
+        mainPage.selectTenRows();
+        int bookCount = mainPage.checkBooksInEveryRow();
+        assertEquals(6, bookCount);
+    }
+
+    @Test
+    public void checkAddAndDeleteBooks(){
+        //Добавить 2 книги
+        int bookCount = mainPage.checkBooksInEveryRow();
+        assertEquals(2, bookCount);
+        mainPage.deleteAllBooks();
+        mainPage.acceptDeleteAllBooks();
+        mainPage.acceptAlert();
+        int finalBookCount = mainPage.checkBooksInEveryRow();
+        assertEquals(0, bookCount);
+
 
     }
 }
