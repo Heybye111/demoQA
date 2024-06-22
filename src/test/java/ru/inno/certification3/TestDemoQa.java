680c15ade40a7cd3370858b4b94dac41b8b15277
Egor Heybye <eshestakov@alfabank.ru>
2024-06-23 04:37:33 +0700

    upd

--- src/test/java/ru/inno/certification3/TestDemoQa.java
+++ src/test/java/ru/inno/certification3/TestDemoQa.java
@@ -21,6 +21,7 @@ import java.nio.file.Paths;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Тесты на DemoQa")
 public class TestDemoQa {
 
     LoginPage loginPage = new LoginPage();
