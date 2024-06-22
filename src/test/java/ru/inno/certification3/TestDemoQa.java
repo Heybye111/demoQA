3ffdac3a3fc785a574c415e0e678c01f36010a19
Egor Heybye <eshestakov@alfabank.ru>
2024-06-23 02:38:37 +0700

    upd

--- src/test/java/ru/inno/certification3/TestDemoQa.java
+++ src/test/java/ru/inno/certification3/TestDemoQa.java
@@ -5,10 +5,11 @@ import com.codeborne.selenide.WebDriverRunner;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import demoQA.helper.Api.ApiSteps;
 import demoQA.helper.Pojo.AddListOfBooks;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.*;
 import demoQA.helper.configHelper;
 import ru.inno.certification3.Pages.LoginPage;
 import ru.inno.certification3.Pages.MainPage;
@@ -21,7 +22,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDemoQa {
-
 
     LoginPage loginPage = new LoginPage();
     MainPage mainPage = new MainPage();
@@ -45,6 +45,10 @@ public class TestDemoQa {
 
     @Test
     @DisplayName("Проверка пустой таблицы")
+    @Description("Сценарий 1. Проверка, что при создании нового пользователя, у него будет пустая таблица с книгами")
+    @Tag("positive")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Heybye")
     public void checkEmptyTable() {
         loginPage.auth();
         mainPage.checkEmptyLines();
@@ -52,6 +56,10 @@ public class TestDemoQa {
 
     @Test
     @DisplayName("Проверка книг в таблице")
+    @Description("Сценарий 2. Проверка, что добавленные книги отображаются в таблице пользователя")
+    @Tag("positive")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Heybye")
     public void checkBooksInTable() throws IOException {
         String books = new String(Files.readAllBytes(Paths.get("src/test/resources/addSixBooks.json")));
         books = books.replace("##userId##", userId);
@@ -65,6 +73,10 @@ public class TestDemoQa {
 
     @Test
     @DisplayName("Проверка удаления книг из таблицы")
+    @Description("Сценарий 3. Проверка, что можно удалить все книги из таблицы пользователя")
+    @Tag("positive")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Heybye")
     public void checkAddAndDeleteBooks() throws IOException, InterruptedException {
         String books = new String(Files.readAllBytes(Paths.get("src/test/resources/addTwoBooks.json")));
         books = books.replace("##userId##", userId);
